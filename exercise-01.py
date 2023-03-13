e611a980990a93f4c103c497102e9a442a49db40
muhammadmarsanda <arsazrkasih9@gmail.com>
2023-03-13 19:31:00 +0700

    tugas prokom 1

---
+++ exercise-01.py
@@ -0,0 +1,8 @@
+print("Muhammad Marsanda Zarkasih")
+print("I0322085")
+print("Tegal")
+print("Rumahku Di dekat jalan tol dan dekat kantor desa mempunyai taman yang luas dan di sebelah rumah ada")
+print("Bisa membuat pabrik dan menerapkan ilmunya")
+
+
+
